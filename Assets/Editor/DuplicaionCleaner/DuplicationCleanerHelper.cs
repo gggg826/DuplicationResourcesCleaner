92160e264f9fbc8e4b8e5273dd0091d4fdcc4562
gggg826 <liushida@126.com>
2018-04-17 16:08:18 +0800

    Add interface of Replace

--- Assets/Editor/DuplicaionCleaner/DuplicationCleanerHelper.cs
+++ Assets/Editor/DuplicaionCleaner/DuplicationCleanerHelper.cs
@@ -6,21 +6,70 @@
 *	purpose:	https://github.com/gggg826/DuplicationResourcesCleaner
 *********************************************************************/
 
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace DuplicaionCleaner
 {
 	public class DuplicationCleanerHelper
 	{
+		private static string DoReplace(string fileFullPath, string strReplace, string strFindPattern, bool isBackup = true)
+		{
+			StringBuilder strError = new StringBuilder();
+			string result = string.Empty;
+			string inputText = string.Empty;
+			string replacement = strReplace;
+			string pat = strFindPattern;
+			Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+
+			try
+			{
+				using (StreamReader sr = new StreamReader(fileFullPath))
+				{
+					inputText = sr.ReadToEnd();
+				}
+
+				if (r.IsMatch(inputText))
+				{
+					if (isBackup == true)
+					{
+						try
+						{
+							File.Copy(fileFullPath, fileFullPath + ".bak");
+						}
+						catch (System.IO.IOException ex)
+						{
+							File.Copy(fileFullPath, fileFullPath + ".bak", true);
+							strError.Append("Error:  " + ex.Message);
+						}
+					}
+					result = r.Replace(inputText, replacement);
+
+					using (StreamWriter sw = new StreamWriter(fileFullPath))
+					{
+						sw.Write(result);
+					}
+				}
+				strError.Append("Log:  " + fileFullPath + "替换完成！！！");
+			}
+			catch (Exception e)
+			{
+				strError.Append("Error:  " + string.Format("The process failed: {0}", e.ToString()));
+			}
+			return strError.ToString();
+		}
+
 		public static void CheckDuplication(AssetBase assetsData)
 		{
-			if(assetsData == null)
+			if (assetsData == null)
 			{
 				return;
 			}
 
-			if(assetsData.AssetDic == null)
+			if (assetsData.AssetDic == null)
 			{
 				assetsData.AssetDic = new Dictionary<string, List<AssetFileData>>();
 			}
@@ -45,12 +94,10 @@ namespace DuplicaionCleaner
 
 		public static void ReadConfig(string path, ref Dictionary<string, List<AssetFileData>> assetDic)
 		{
-
 		}
 
 		public static void SaveConfig(string path, Dictionary<string, List<AssetFileData>> assetDic)
 		{
-
 		}
 
 		public static string GetPathByRelative(string path)
